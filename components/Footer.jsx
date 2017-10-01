f1e39b96bac7c45f30bad9b0aacf096db3f8b8e9
Aleksands Shaytanov <rdischain@yandex.ru>
2017-10-01 19:00:14 +0400

    add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+const Footer = () => (
+  <footer className={'footer navbar-fixed-bottom'}>
+    <div className={'container-fluid'}>
+      <p className={'text-muted text-center'}>dischain, 2017</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
